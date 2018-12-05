77ac82e6f96477bf493af35879fd363d146521b1
Manuel Lecaro <mnlecaro@gmail.com>
2018-12-05 11:48:03 -0500

    cambios en scrap

--- scrapMotion.py
+++ scrapMotion.py
@@ -1,18 +1,18 @@
 import serial
 import math
-from numpy import *
+import numpy as numpy
 
 SERVO_MIN = math.radians(-80)
 SERVO_MAX = math.radians(80) 
 THETA = [0.0,0.0,0.0, 0.0,0.0,0.0]
 BETA = [0,math.pi,2*math.pi/3,-math.pi/3,-2*math.pi/3,math.pi/3]
 
-P[2][6]= [
+P = [
   [-37.62,37.62,91.20,51.83,-51.83,-91.20],
   [-91.60,-91.60,0,67.77,67.77,0]
   ]
 
-RE[3][6] = [
+RE = [
   [-74.70,74.70,84.66,20,-20,-84.66],
   [-62.95,-62.95,-45.48,84.80,84.80,-45.48],
   [0,0,0,0,0,0]
@@ -21,7 +21,7 @@ RE[3][6] = [
 '''
 L1-effective length of servo arm, L2 - length of base and platform connecting arm
 z_home - height of platform above base, 0 is height of servo arms'''
-servo_mult=400/(pi/4)
+servo_mult=400/(math.pi/4)
 L1 = 20
 L2 = 170
 z_home = 165
@@ -29,8 +29,8 @@ z_home = 165
 '''arrays used for servo rotation calculation
 H[]-center position of platform can be moved with respect to base, this is
 translation vector representing this move'''
-M = array([][])
-rxp = array([][])
+M = [[],[]]
+rxp = [[],[]]
 T = []
 H = [0,0,z_home]
 def base_attach_point(i, th, position_p):
@@ -42,15 +42,15 @@ def getAlpha(i):
    q= [] 
    dl = []
    dl2 = []
-   min=servo_min
-   max=servo_max
-   th=theta_a[i]
+   min=SERVO_MIN
+   max=SERVO_MAX
+   th=THETA[i]
 
    while(n<20):
     #calculation of position of base attachment point (point on servo arm where is leg connected)
       q.append(base_attach_point(i, th, 0))
       q.append(base_attach_point(i, th, 1))
-      q.append(L1*sin(th))
+      q.append(L1*math.sin(th))
       
     #calculation of distance between according platform attachment point and base attachment point
       dl[0] = rxp[0][i] - q[0]
@@ -59,7 +59,7 @@ def getAlpha(i):
       dl2 = math.sqrt(dl[0]*dl[0] + dl[1]*dl[1] + dl[2]*dl[2])
 
     #if this distance is the same as leg length, value of theta_a is corrent, we return it
-      if(math.abs(L2-dl2)<0.01):
+      if(abs(L2-dl2)<0.01):
          return th
     #//if not, we split the searched space in half, then try next value
       if(dl2<L2):
@@ -67,10 +67,8 @@ def getAlpha(i):
       else:
          min=th
       n+=1
-      
-    if(max==SERVO_MIN or min==SERVO_MAX):
+      if(max==SERVO_MIN or min==SERVO_MAX):
         return th
-    th = min+(max-min)/2
-   return th
-}
+      th = min+(max-min)/2
+      return th
 
