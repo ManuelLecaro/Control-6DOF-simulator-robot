a01d030397fc63205f5868889937e8f784cc9a97
Daniel Arroyo <darroyo@espol.edu.ec>
2018-12-16 18:46:18 -0500

    Changes in the GUI. Lacks connectiity of GUI with the upload and conversion logic.

--- Main.py
+++ Main.py
@@ -5,26 +5,43 @@ uses PyQT5
 
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+import PyQt5.QtCore as cor
+import PyQt5.QtGui as gui
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QBoxLayout, QAction
 
 class Main(QWidget):
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
-    def initUI(self):
-        lbl = QLabel('Bienvenido al robot simuldor de sismos', self)
+    def openFileChooser(self):
+        print("TODO: Open a window to choose the .zip file")
 
-        btn = QPushButton('Subir archivos', self)
-        btn.resize(btn.sizeHint())
+    # Function to add widgets to the main window
+    def initUI(self):
+        boxlayout = QBoxLayout(QBoxLayout.TopToBottom)
+        self.setLayout(boxlayout)
+
+        lblWelcome = QLabel('Welcome to the earthquake simulation system')
+        lblWelcome.setFont(gui.QFont("Verdana", 14))
+        lblSubtitle = QLabel('Download the .zip files via strongmotioncenter.org and upload them here')
+        lblSubtitle.setFont(gui.QFont("Verdana",10))
+
+        btnUpload = QPushButton('Upload files')
+        btnUpload.setFixedSize(100, 40)
+        btnUpload.setFont(gui.QFont("Verdana",10))
+        btnUpload.clicked.connect(self.openFileChooser)
+
+
+        boxlayout.addWidget(lblWelcome, 0, cor.Qt.AlignCenter)
+        boxlayout.addWidget(lblSubtitle, 0, cor.Qt.AlignCenter)
+        boxlayout.addWidget(btnUpload, 0, cor.Qt.AlignCenter)
+        boxlayout.setAlignment(cor.Qt.AlignCenter)
+        boxlayout.setSpacing(20)
 
         self.resize(800,500)
         self.setWindowTitle('Earthquake Simulator')
         self.show()
-
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
